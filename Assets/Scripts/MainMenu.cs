203927bf71ff7ab628c3c5d5b980af39db3f0289
kirvy <kirvy810@gmail.com>
2023-05-23 22:44:13 +0900

    Refactor MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public void OnClickNew()
+    public void OnClickStart()
     {
 #if UNITY_EDITOR
-        Debug.Log("New Game");
+        Debug.Log("Clicked Start Button");
 #endif
     }
 
     public void OnClickLoad()
     {
 #if UNITY_EDITOR
-        Debug.Log("Load");
+        Debug.Log("Clicked Load Button");
 #endif
     }
 
     public void OnClickConfig()
     {
 #if UNITY_EDITOR
-        Debug.Log("Config");
+        Debug.Log("Clicked Config Button");
 #endif
     }
 
     public void OnClickQuit()
     {
 #if UNITY_EDITOR
-        Debug.Log("Quit");
+        Debug.Log("Clicked Quit Button");
 #else
         Application.Quit();
 #endif
