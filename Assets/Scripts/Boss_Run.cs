a519dcd30200da35ca0b9caad431615e4a4880f6
SungJiYoon <zxq0158@gmail.com>
2023-05-31 22:35:21 +0900

    Monster(2023.05.31.)
    보스가 캐릭터를 따라가는데 좌우 뒤집는거, 애니메이션 파라미터 추가, 공격 애니메이션 트리거 추가, 공격했을 때 플레이어가 당할거 함수 하다말음

--- Assets/Scripts/Boss_Run.cs
+++ Assets/Scripts/Boss_Run.cs
@@ -5,29 +5,39 @@ using UnityEngine;
 public class Boss_Run : StateMachineBehaviour
 {
     [SerializeField]
-    private float speed = 2.5f;
+    private float speed = 1f;
+    [SerializeField]
+    private float attackRange = 3f;
 
     Transform player;
     Rigidbody rb;
+    Boss boss;
 
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody>();
+        boss = animator.GetComponent<Boss>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        boss.LookAtPlayer();
+        Vector2 target = new Vector2(player.position.x, rb.position.z);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
+
+        if(Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("BasicAttack");
+        }
     }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.ResetTrigger("BasicAttack");
     }
 }
