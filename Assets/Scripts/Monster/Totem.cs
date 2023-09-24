ce9a85ad2314956c76a5e076244b139978bb92a2
Sung JI Yoon <66246253+SungJiYoon@users.noreply.github.com>
2023-09-24 19:28:28 +0900

    0,1층 몬스터 배치

---
+++ Assets/Scripts/Monster/Totem.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem : MonoBehaviour
+{
+    [SerializeField]
+    protected float maxHealth;
+    [SerializeField]
+    protected float curHealth;
+
+    BoxCollider boxCollider;
+    MeshRenderer mat;
+
+    void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<MeshRenderer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Sword")
+        {
+            Sword sword = other.GetComponent<Sword>();
+            curHealth -= sword.damage;
+
+            Debug.Log("Sword : " + curHealth);
+            StartCoroutine(OnDamage());
+        }
+    }
+
+    IEnumerator OnDamage()
+    {
+        mat.material.color = Color.red;
+
+        yield return new WaitForSeconds(0.1f);
+
+        if (curHealth > 0)
+        {
+            mat.material.color = Color.white;
+        }
+        else //»ç¸Á
+        {
+            mat.material.color = Color.gray;  
+            Destroy(gameObject, 2);
+
+        }
+    }
+}
