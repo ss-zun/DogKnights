9c37db71085ca18d9272146edb44ee5a8d440e9e
Sung JI Yoon <66246253+SungJiYoon@users.noreply.github.com>
2023-10-04 00:12:32 +0900

    isDead를 public으로 변경

--- Assets/Scripts/Monster/MonsterManager.cs
+++ Assets/Scripts/Monster/MonsterManager.cs
@@ -14,6 +14,9 @@ public class MonsterManager : MonoBehaviour
     GameObject golem;
     [SerializeField]
     GameObject boss;
+   
+    [SerializeField]
+    private float createTime; //몬스터를 발생시킬 주기
 
     private int totalMonsterCount;
     private int countTotem;
@@ -22,7 +25,8 @@ public class MonsterManager : MonoBehaviour
     private int countGolem;
     private int countBoss;
 
-    private int currentFloor;
+    private int currentFloor; // 현재 몇 층인지
+    private bool isGameOver = false; // 게임종료 여부
 
     public int TotalMonsterCount
     {
@@ -35,8 +39,9 @@ public class MonsterManager : MonoBehaviour
         MonsterSpawner(currentFloor);
     }
     private void Update()
-    {    
-        if(currentFloor != getFloorState())
+    {
+        isGameOver = GameManager.Instance.Player.isDead;
+        if (currentFloor != getFloorState())
             MonsterSpawner(currentFloor);
     }
 
@@ -50,6 +55,7 @@ public class MonsterManager : MonoBehaviour
         switch (floor)
         {
             case 0:
+                totalMonsterCount = countTotem;
                 break;
             case 1:
                 break;
@@ -63,6 +69,30 @@ public class MonsterManager : MonoBehaviour
                 break;
             case 10:
                 break;
+        }
+    }
+
+    IEnumerator CreateMonster()
+    {
+        while (!isGameOver)
+        {
+            //현재 생성된 몬스터 개수 산출
+            int monsterCount = (int)GameObject.FindGameObjectsWithTag("Monster").Length;
+
+            if (monsterCount < totalMonsterCount)
+            {
+                //몬스터의 생성 주기 시간만큼 대기
+                yield return new WaitForSeconds(createTime);
+
+                //불규칙적인 위치 산출
+                //int idx = Random.Range(1, points.Length);
+                //몬스터의 동적 생성
+                //Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
         }
     }
 }
