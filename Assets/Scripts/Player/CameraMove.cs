0d20f2e8e978476e48641fade7e3095476b2e13d
김연경 <kimi070464@gmail.com>
2024-01-04 21:02:09 +0900

    24.01.04. camera에러 수정

--- Assets/Scripts/Player/CameraMove.cs
+++ Assets/Scripts/Player/CameraMove.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {   
     public GameObject Target;
-    float offsetX = 0.0f;
-    float offsetY = 0.0f;
-    float offsetZ = -16.0f;
+    public float offsetX = 0.0f;
+    public float offsetY = 4.0f;
+    public float offsetZ = -16.0f;
 
     public float CameraSpeed = 10.0f;
     Vector3 TargetPos;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,7 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 camPos = Target.transform.position;
+        //Vector3 camPos = Target.transform.position;
         if (Target.GetComponent<Player>().isMapPuzzle)
         {
             cameraToVertical(16f);
@@ -33,29 +34,38 @@ public class CameraMove : MonoBehaviour
         }
         //camPos.z = camPos.z-16;
         //transform.position = camPos;
+
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * CameraSpeed);
     }
 
     void FixedUpdate(){
-        TargetPos = new Vector3(
-            Target.transform.position.x + offsetX,
-            Target.transform.position.y + offsetY,
-            Target.transform.position.z + offsetZ
-        );
+        //TargetPos = new Vector3(
+        //    Target.transform.position.x + offsetX,
+        //    Target.transform.position.y + offsetY,
+        //    Target.transform.position.z + offsetZ
+        //);
 
         transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * CameraSpeed);
     }
 
     void cameraToHorizontal(float offset)
     {
-        offsetY = 0.0f;
-        offsetZ = offset;
+
+        TargetPos = new Vector3(
+            Target.transform.position.x + offsetX,
+            Target.transform.position.y + offsetY,
+            Target.transform.position.z + offset
+        );
         transform.rotation = Quaternion.Euler(0,0, 0);
     }
 
     void cameraToVertical(float offset)
     {
-        offsetY = offset;
-        offsetZ = 0.0f;
+        TargetPos = new Vector3(
+            Target.transform.position.x + offsetX,
+            Target.transform.position.y + offset,
+            Target.transform.position.z + 0.0f
+        ) ;
         transform.rotation = Quaternion.Euler(90f, 0, 0);
     }
 
